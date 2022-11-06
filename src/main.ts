14ca5da479a3b63785c2ad24f964648e8f6eb614
Tree <lshf1437@163.com>
2022-11-06 21:03:45 +0800

    fix commonjs require problem

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
-// import Antd from 'ant-design-vue'
-// Vue.use(Antd)
 import antdRegister from './utils/antd'
 import 'normalize.css'
 
